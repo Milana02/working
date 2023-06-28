aa1d8670798628dcd3b3655ca2afb8720ea6488d
Milana02 <137777387+Milana02@users.noreply.github.com>
2023-06-28 13:32:25 +0300

    Update mailer.php

--- #2/mailer.php
+++ #2/mailer.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Services\User;
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
+
+require_once 'phpmailer\PHPMailer.php';
+require_once 'phpmailer\Exception.php';
+require_once 'phpmailer\SMTP.php';
+
 
 //require '../../../vendor/autoload.php';
 
